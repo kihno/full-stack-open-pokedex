0e5478b3ca76b7a998b98890eeb7d2caaa8a815e
kihno <stistyle13@gmail.com>
2024-03-04 18:13:10 -0800

    Fix typo in test link

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -5,7 +5,7 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000/pokemn/ivysaur')
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
     cy.contains('chlorophyll')
   })
 })
